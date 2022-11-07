7be78bad35317b9695837311628382a6ce5f916f
Milliella <115160750+Milliella@users.noreply.github.com>
2022-11-07 11:14:09 +0000

    edit

--- audio-player-task/javascript/audio-player.js
+++ audio-player-task/javascript/audio-player.js
@@ -1,4 +1,6 @@
 console.log("hello world");
+
+
 const currentTime = document.getElementById("current-time");
 const totalTime = document.getElementById("total-time");
 const playPause = document.getElementById("play-pause");
@@ -11,6 +13,7 @@ console.log(obj.volume);
 obj.value = 1;
 let isSeeking = false;
 
+// listens for when the play / pause button is clicked. if the pause symbol is showing, play the audio
 playPause.onclick = function(){
     if(audio.paused){
         audio.play();
@@ -19,20 +22,27 @@ playPause.onclick = function(){
     }
 
 }
+// the audio duration that the seek bar displays, listens for when the selected audio is fully loaded 
 audio.oncanplaythrough = function(){
     seekBar.disabled = false;
 }
+// click the play button, it then displays the pause button to indicate that the audio is playing and you can pause it.
+// listens for when its clicked
 audio.onplay = function(){
     playPause.src = "images/pause.svg";
 }
+// displays the play button to indicate the audio is paused, listens for when the pause button is clicked
 audio.onpause = function(){
     playPause.src = "images/play.svg";
 }
+// takes the total time element and the current time element and applys whats currently happing to them :
+// the current time signiture of the track and the total length of the track, uses that to control the audio when the seek bar is used
 audio.onloadedmetadata = function(){
     totalTime.innerHTML = formatTime(audio.duration);
     currentTime.innerHTML = formatTime(0);
     seekBar.max = Math.floor(audio.duration);
 }
+
 audio.ontimeupdate = function(){
     currentTime.innerHTML = formatTime(audio.currentTime);
     if(!isSeeking){
@@ -40,22 +50,40 @@ audio.ontimeupdate = function(){
     }
 
 }
+// when the audio ends display the play button to indicate the track has ended and you can play it again, if the song ending is true- display the play button
 audio.onended = function(){
     currentTime.innerHTML = formatTime(0);
     seekBar.value = 0;
     playPause.src = "images/play.svg";
 
 }
+// event listener: listens for interaction with seek bar 
 seekBar.oninput = function(){
     isSeeking = true;
 }
+// when the seek bar is moved right or left, take the position of the seek bar and the time signiture it represents and apply that to the track- changing the part of the song you're listening to
 seekBar.onchange = function(){
     audio.currentTime = seekBar.value;
     isSeeking = false;
 }
-for (let i = 0; i < audio.audioTracks.length; i += 1){
-    audio.audioTracks[1].enabled = false;
-}
+
+// VOLUME CONTROL ATTEMPTS
+
+// volumeControl.oninput = function(){
+//     isSeeking=true;
+// }
+// volumeControl.onchange = function(){
+//     audio.volume = volumeControl.value;
+//     isSeeking = false;
+// }
+// volumeControl.addEventListener ("mousemove", setvolme);
+// function setvolme(){
+//     audio.volume = volumeControl.value / 100;
+// }
+
+// for (let i = 0; i < audio.audioTracks.length; i += 1){
+//     audio.audioTracks[1].enabled = false;
+// }
 // volumeControl.onchange = function(){
 //     audio.volume = volumeControl.value;
 //     isSeeking = false;
@@ -81,7 +109,7 @@ for (let i = 0; i < audio.audioTracks.length; i += 1){
 
 
 
-
+// controls the current time that is displayed on the player 
 function formatTime(secs) {
     let hours = Math.floor(secs / 3600);
     let minutes = Math.floor((secs - (hours * 3600)) / 60);
